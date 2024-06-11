20b25b016d255b3649d090edb46344300dab14aa
mate <matekopaliani12@gmail.com>
2024-06-11 16:59:44 +0400

    added uri

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
             <KindeProvider
                 clientId="d4f34628d9e44bc79a48fb30c35ce4d8"
                 domain="https://hotelmanagment.kinde.com"
-                redirectUri="http://localhost:3000"
-                logoutUri="http://localhost:3000"
+                logoutUri={window.location.origin}
+                redirectUri={window.location.origin}
             >
                 <Nav />
             </KindeProvider>
