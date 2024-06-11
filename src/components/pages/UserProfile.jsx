42a7906e9d03b6c5bc7f44cbc65d77e7b4ed25e1
mate <matekopaliani12@gmail.com>
2024-06-11 23:23:45 +0400

    adding important stuff

--- src/components/pages/UserProfile.jsx
+++ src/components/pages/UserProfile.jsx
@@ -2,10 +2,11 @@ import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import './css/UserProfile.css';
 import { FaRegUserCircle } from "react-icons/fa";
 import { TfiEmail } from "react-icons/tfi";
-
+import { useState } from "react";
 
 const UserProfile = () => {
     const { user, isAuthenticated, isLoading } = useKindeAuth();
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
 
     if (isLoading) {
         return <p>Loading...</p>;
@@ -14,19 +15,26 @@ const UserProfile = () => {
     return (
         isAuthenticated && (
             <div className="user-profile">
-                <div className="circle">
-                    <FaRegUserCircle className="coloring" size={40} />
-                    <div className="dropdown">
-                        <p>
-                            {user.family_name}
-                        </p>
-                        <p>
-                            <TfiEmail />
-
-                            {user.email}
-                        </p>
-                    </div>
+                <div
+                    className="user-icon"
+                    onClick={() => setDropdownVisible(!isDropdownVisible)}
+                    onMouseEnter={() => setDropdownVisible(true)}
+                    onMouseLeave={() => setDropdownVisible(false)}
+                >
+                    <FaRegUserCircle className="icon" size={40} />
                 </div>
+                {isDropdownVisible && (
+                    <div className="dropdown-menu">
+                        <div className="dropdown-item">
+                            <FaRegUserCircle className="dropdown-icon" />
+                            <span>{user.family_name}</span>
+                        </div>
+                        <div className="dropdown-item">
+                            <TfiEmail className="dropdown-icon" />
+                            <span>{user.email}</span>
+                        </div>
+                    </div>
+                )}
             </div>
         )
     );
